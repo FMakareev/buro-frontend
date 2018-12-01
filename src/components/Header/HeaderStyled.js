097db83cd1037a024349c6e7b307d7f5583ca168
FMakareev <f.makareev@compaero.ru>
2018-12-01 21:31:25 +0300

    Изменил цвет нижнего бордера

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import {Flex} from "../Flex/Flex";
 
 export const HeaderWrapper = styled(Flex)`
-  border-bottom: 1px solid ${({theme}) => theme.colors.color5}; 
+  border-bottom: 1px solid rgba(162, 162, 162, 0.2); 
   background-color: #fff;
 `;
