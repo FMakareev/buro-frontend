afcd809408b0eee27ffd92bec052878dbd6c46eb
NikolaZh <djozefs@gmail.com>
2018-12-02 17:51:56 +0300

    create

---
+++ src/modules/user/components/HelpText.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Text } from '../../../components/Text/Text';
+
+const StyledText = styled(Text)`
+  font-family: ${props => props.theme.fontFamily.regular};
+  font-size: ${props => props.theme.fontSizes[4]}px;
+  line-height: ${props => props.theme.fontSizes[6]}px;
+  color: ${props => props.theme.colors.color5};
+
+  & > a {
+    color: ${props => props.theme.colors.color8};
+    text-decoration: none;
+  }
+`;
+
+export const HelpText = ({ children }) => <StyledText>{children}</StyledText>;
+
+export default HelpText;
