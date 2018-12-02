330c1272995915fe626ffae480ed965269f8133b
FMakareev <f.makareev@compaero.ru>
2018-12-02 19:19:30 +0300

    Cjplfk rjvgjytyn j,thnrf lkz ghtkjflthf

---
+++ src/components/PreloaderWrapper/PreloaderWrapper.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import {Box} from "../Box/Box";
+
+export const PreloaderWrapper = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%; 
+  background-color: rgba(255, 255, 255, 0.6); 
+  transition: .225s all;
+`;
+
+export default PreloaderWrapper;
