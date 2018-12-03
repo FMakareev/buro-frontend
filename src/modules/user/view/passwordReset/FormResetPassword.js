e09375e831c72490e62f0013cf6296e9a8400e25
FMakareev <f.makareev@compaero.ru>
2018-12-03 18:05:58 +0300

    Добавил кнопку ошибки.

--- src/modules/user/view/passwordReset/FormResetPassword.js
+++ src/modules/user/view/passwordReset/FormResetPassword.js
@@ -14,6 +14,8 @@ import { Text } from '../../../../components/Text/Text';
 import { SvgArrowLeft } from '../../../../components/Icons/SvgArrowLeft';
 import { SpeedingWheel } from '../../../../components/SmallPreloader/SmallPreloader';
 import { PreloaderWrapper } from '../../../../components/PreloaderWrapper/PreloaderWrapper';
+import isEmail from "../../../../utils/validation/isEmail";
+import {ReloadIcon} from "../../components/ReloadIcon";
 
 const FormStyled = styled(Form)`
   position: relative;
@@ -34,7 +36,7 @@ export class FormResetPassword extends Component {
     new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve(true);
-      }, 5000);
+      }, 1000);
     }).then(() => {
       throw new SubmissionError({
         _error: 'Connection error!',
@@ -53,7 +55,7 @@ export class FormResetPassword extends Component {
     } = this.props;
     return (
       <FormStyled onSubmit={handleSubmit(this.submit)}>
-        {!submitSucceeded && !submitFailed && (
+        {(submitFailed || (!submitSucceeded && !submitFailed)) && (
           <Flex justifyContent={'center'} width={'100%'} flexDirection={'column'}>
             <Box width={'100%'} mb={6}>
               <Field
@@ -61,27 +63,77 @@ export class FormResetPassword extends Component {
                 component={TextFieldWithIcon}
                 placeholder={'Email address'}
                 type={'email'}
+                validate={[required,isEmail]}
                 icon={<EmailIcon />}
               />
             </Box>
-            <Box width={'100%'}>
+            {
+              !submitFailed &&
+              <Box width={'100%'}>
+                <ButtonWithImageError
+                  type={'submit'}
+                  variant={'primary'}
+                  size={'medium'}
+                  fontSize={6}
+                  error={error}
+                  width={'100%'}
+                  iconRight={
+                    <Text fontSize={12} lineHeight={0}>
+                      <SvgArrowRight />
+                    </Text>
+                  }
+                  disabled={pristine || submitting || invalid}>
+                  Send password reset mail
+                </ButtonWithImageError>
+              </Box>
+            }
+
+            {submitFailed && (
+              <Box width={"100%"}>
+                <ButtonWithImageError
+                  type={"submit"}
+                  variant={"error"}
+                  size={"medium"}
+
+                  error={error}
+                  iconRight={
+                    <Text fontSize={12} lineHeight={0}>
+                      <ReloadIcon/>
+                    </Text>
+                  }
+                >
+                  Try again
+                </ButtonWithImageError>
+              </Box>
+            )}
+          </Flex>
+        )}
+
+        {submitSucceeded && !submitFailed && (
+          <Box width="100%">
+            <Box width={'100%'} mb={6}>
+              <Text fontSize={6} lineHeight={12} color={'color1'} fontFamily={'medium'}>
+                Password change link was has been sent to your email adress. Click the link in the
+                email to reset your password.
+              </Text>
+            </Box>
+            <Link to={'/login'}>
               <ButtonWithImageError
-                type={'submit'}
                 variant={'primary'}
                 size={'medium'}
                 fontSize={6}
-                error={error}
+                pl={4}
                 width={'100%'}
-                iconRight={
+                error={error}
+                iconLeft={
                   <Text fontSize={12} lineHeight={0}>
-                    <SvgArrowRight />
+                    <SvgArrowLeft />
                   </Text>
-                }
-                disabled={pristine || submitting || invalid}>
-                Send password reset mail
+                }>
+                Go to main screen
               </ButtonWithImageError>
-            </Box>
-          </Flex>
+            </Link>
+          </Box>
         )}
 
         {submitting && (
@@ -92,33 +144,6 @@ export class FormResetPassword extends Component {
           </PreloaderWrapper>
         )}
 
-        {(submitSucceeded || submitFailed) && (
-          <Flex justifyContent={'center'} width={'100%'} flexDirection={'column'}>
-            <Box width={'100%'} mb={6}>
-              <Text fontSize={6} lineHeight={12} color={'color1'} fontFamily={'medium'}>
-                Password change link was has been sent to your email adress. Click the link in the
-                email to reset your password.
-              </Text>
-            </Box>
-            <Box width={'100%'}>
-              <Link to={'/login'}>
-                <ButtonWithImageError
-                  variant={'primary'}
-                  size={'medium'}
-                  fontSize={6}
-                  width={'100%'}
-                  error={error}
-                  iconLeft={
-                    <Text fontSize={12} lineHeight={0}>
-                      <SvgArrowLeft />
-                    </Text>
-                  }>
-                  Send password reset mail
-                </ButtonWithImageError>
-              </Link>
-            </Box>
-          </Flex>
-        )}
       </FormStyled>
     );
   }
