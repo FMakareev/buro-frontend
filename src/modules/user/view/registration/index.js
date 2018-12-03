612348e490a3b11c1eaf261db950228cbc9deb15
FMakareev <f.makareev@compaero.ru>
2018-12-03 17:13:16 +0300

    Поменял отступы у блоков. Добавил позиционирование контейнеру.

--- src/modules/user/view/registration/index.js
+++ src/modules/user/view/registration/index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
-import { Box } from '../../../../components/Box/Box';
+import {Box} from '../../../../components/Box/Box';
 
-import { Wrapper } from '../../components/Wrapper';
-import { Title } from '../../components/Title';
-import { HelpText } from '../../components/HelpText';
+import {Wrapper} from '../../components/Wrapper';
+import {Title} from '../../components/Title';
+import {HelpText} from '../../components/HelpText';
 
-import { FormUserRegistration } from './FormUserRegistration';
+import {FormUserRegistration} from './FormUserRegistration';
 
 export class RegistrationPage extends Component {
   static propTypes = {};
@@ -25,10 +25,14 @@ export class RegistrationPage extends Component {
 
   render() {
     return (
-      <Wrapper ml={100} mt={120} maxWidth={360}>
-        <Title>Sign up</Title>
-        <FormUserRegistration />
-        <Box mt="16px">
+      <Wrapper position={'relative'} ml={100} mt={120} maxWidth={360}>
+        <Box mb={6}>
+          <Title>Sign up</Title>
+        </Box>
+        <Box mb={6}>
+          <FormUserRegistration/>
+        </Box>
+        <Box>
           <HelpText>
             Already have an account? <Link to="/login">Sign in</Link>
           </HelpText>
