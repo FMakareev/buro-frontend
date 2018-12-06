0d7ca5fbe6506c1d77fd975b1c9b1d8a1f71e9e9
FMakareev <f.makareev@compaero.ru>
2018-12-06 15:37:53 +0300

    Создал компонент иконки банка.

---
+++ src/components/Icons/SvgBank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const SvgBank = () => (<svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37259 18.6274 0 12 0C5.37255 0 0 5.37259 0 12C0 18.6274 5.37255 24 12 24ZM12 23.25C5.79671 23.25 0.75 18.2033 0.75 12C0.75 5.79675 5.79671 0.75 12 0.75C18.2033 0.75 23.25 5.79675 23.25 12C23.25 18.2033 18.2033 23.25 12 23.25Z" fill="inherit"/>
+    <path d="M5.02442 8L12 3.30142L18.9756 8H5.02442Z" stroke="inherit" strokeWidth="0.5"/>
+    <rect x="4" y="17" width="16" height="0" stroke="inherit"/>
+    <rect x="-0.125" y="0.125" width="12.25" height="0.25" transform="matrix(1 0 0 -1 6 9.25)" stroke="inherit" strokeWidth="0.25"/>
+    <rect x="0.125" y="0.125" width="5.7378" height="0.469512" transform="matrix(-6.99382e-08 -1 -1 2.73196e-08 6.76172 15.5337)" stroke="inherit" strokeWidth="0.25"/>
+    <rect x="0.125" y="0.125" width="5.7378" height="0.469512" transform="matrix(-6.99382e-08 -1 -1 2.73196e-08 10.4937 15.5337)" stroke="inherit" strokeWidth="0.25"/>
+    <rect x="0.125" y="0.125" width="5.7378" height="0.469512" transform="matrix(-6.99382e-08 -1 -1 2.73196e-08 17.9575 15.5337)" stroke="inherit" strokeWidth="0.25"/>
+    <rect x="0.125" y="0.125" width="5.7378" height="0.469512" transform="matrix(-6.99382e-08 -1 -1 2.73196e-08 14.2256 15.5337)" stroke="inherit" strokeWidth="0.25"/>
+  </svg>
+
+);
+export default SvgBank;
