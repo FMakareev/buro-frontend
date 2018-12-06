d1f5843491f4a8432463e9c05338f2256fc29ecf
FMakareev <f.makareev@compaero.ru>
2018-12-06 13:25:38 +0300

    Заменил варианты статусов на константы

--- src/apollo/graphql/query/documentItem.js
+++ src/apollo/graphql/query/documentItem.js
@@ -1,11 +1,12 @@
 // генерирует объект документа для таблицы документов
 import faker from "faker";
+import {STATUS_APPROVAL, STATUS_NOT_APPROVAL, STATUS_PENDING} from "../../../shared/statuses";
 
 
 export const documentItem = () => {
   return {
     id: faker.random.uuid(),
-    status: faker.random.arrayElement(['pending_approval', 'not_approval', 'approval']),
+    status: faker.random.arrayElement([STATUS_PENDING, STATUS_NOT_APPROVAL, STATUS_APPROVAL]),
     date: faker.date.past().toUTCString(),
     file: faker.image.imageUrl(),
   }
