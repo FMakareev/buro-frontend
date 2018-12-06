63c6b7100d77c2c823f85b4acfc97f8cd1273a01
FMakareev <f.makareev@compaero.ru>
2018-12-06 15:10:59 +0300

    Добавил поддержку массивов ролей и проверку их. Добавил поддержку компонента ошибки доступа. Обновил jsdoc.

--- src/components/CheckAuthorization/CheckAuthorization.js
+++ src/components/CheckAuthorization/CheckAuthorization.js
@@ -2,21 +2,27 @@ import React from 'react';
 import {Redirect} from "react-router-dom";
 // TODO: доделать редирект на страницу с ошибкой доступа или просто поставить компонент
 /**
- * @param {string} role -
+ * @param {array} role - массив ролей у которых есть доступ к компоненту
+ * @param {object} AccessDeniedCallback - компонент кторый должен быть отображен в случае ошибки
  * @desc
  * */
-export const CheckAuthorization = (role) => WrappedComponent => {
+export const CheckAuthorization = (role = [], AccessDeniedCallback = null) => WrappedComponent => {
 
   return (props) => {
-    const {user} = props;
-    if (!user || (user && user.error)) {
-      return <Redirect to={'/logout'}/>
-    }
-    if(user && user.initLoading){
-      return null;
-    }
-    if((user && role) && user.role !== role){
-      return <div>Доступ закрыт</div>;
+    try {
+      const {user} = props;
+      console.log('user: ', user);
+      if (!user || (user && user.error)) {
+        return <Redirect to={'/logout'}/>
+      }
+      if (user && user.initLoading) {
+        return null;
+      }
+      if ((user && role.length) && !role.filter(item => item === user.role).length) {
+        return AccessDeniedCallback;
+      }
+    } catch (error) {
+      console.log(error);
     }
     return (<WrappedComponent {...props} />);
   }
