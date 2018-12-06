af5ba37d2de3c72f70c9aaa7bd6b6d800caf2df7
FMakareev <f.makareev@compaero.ru>
2018-12-06 14:55:10 +0300

    Вынес стили компонента HeaderNotification в отдельный файл.

---
+++ src/components/HeaderNotification/HeaderNotificationStyled.js
@@ -0,0 +1,29 @@
+import {Text} from "../Text/Text";
+import styled from "styled-components";
+
+export const CircleCount = styled(Text)`
+  position: absolute;
+  top: -3px;
+  right: -3px;
+  min-width: 16px;
+  min-height: 16px;
+  text-align: center;
+  border-radius: 500px;
+  flex-shrink: 0;
+  font-size: 14px;
+  line-height: 16px;
+  padding: 0 4px;
+  background-color: ${({theme}) => theme && theme.colors && theme.colors.color1};
+  color: #ffffff;
+`;
+export const ButtonStyled = styled(Text)`
+  position: relative;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  height: 40px;
+  width: 40px;
+  line-height: 0;
+  cursor: pointer;
+  outline: none;
+`;
