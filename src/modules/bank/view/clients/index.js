5d3a162760ba2358ab5edf2d915fdb4d31676bc1
FMakareev <f.makareev@compaero.ru>
2018-12-06 20:49:27 +0300

    Удолил лишний код

--- src/modules/bank/view/clients/index.js
+++ src/modules/bank/view/clients/index.js
@@ -6,16 +6,16 @@ import { Text } from '../../../../components/Text/Text';
 import { ButtonBase } from '../../../../components/ButtonBase/ButtonBase';
 import { SvgCancelRequest } from '../../../../components/Icons/SvgCancelRequest';
 import { ButtonWithImage } from '../../../../components/ButtonWithImage/ButtonWithImage';
-import { makeData } from '../../../buro/helpers/utils';
+import { makeData } from '../../../bureau/helpers/utils';
 import { ReactTableStyled } from '../../../../components/ReactTableStyled/ReactTableStyled';
-import { FormDocumentUpload } from '../../../buro/components/FormDocumentUpload/FormDocumentUpload';
-import { Modal } from '../../../../components/Modal/Modal';
 
 import UserListQuery from './UserListQuery.graphql';
 
 import { STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL } from '../../../../shared/statuses';
+import {CheckAuthorization} from "../../../../components/CheckAuthorization/CheckAuthorization";
+import {ROLE_BANK} from "../../../../shared/roles";
 
-const columns = ({ onOpenFormUpdateDoc }) => [
+const columns = () => [
   {
     id: 'Client',
     Header: 'Client',
@@ -109,6 +109,7 @@ const columns = ({ onOpenFormUpdateDoc }) => [
   },
 ];
 
+@CheckAuthorization([ROLE_BANK])
 export class ClientsPage extends Component {
   static propTypes = {};
 
@@ -121,20 +122,9 @@ export class ClientsPage extends Component {
 
   get initialState() {
     return {
-      // статус открытия модального окна
-      isOpen: false,
-      // id пользователя к которому крепится окумент
-      id: null,
       data: makeData(100),
     };
   }
-
-  onOpenFormUpdateDoc = id => this.setState(() => ({ id, isOpen: true }));
-
-  toggleModal = () => {
-    console.log('toggleModal');
-    this.setState(state => ({ isOpen: !state.isOpen }));
-  };
 
   render() {
     const { isOpen, id } = this.state;
@@ -154,19 +144,12 @@ export class ClientsPage extends Component {
                 }
                 data={loading ? [] : data.userDocumentList}
                 filterable
-                columns={columns({
-                  onOpenFormUpdateDoc: this.onOpenFormUpdateDoc,
-                })}
+                columns={columns()}
               />
             );
           }}
         </Query>
 
-        {isOpen && (
-          <Modal toggleModal={this.toggleModal}>
-            <FormDocumentUpload toggleModal={this.toggleModal} id={id} />
-          </Modal>
-        )}
       </Container>
     );
   }
