cb1ec003ae2ca411262b60dae3a7f0e4db122c4f
NikolaZh <djozefs@gmail.com>
2018-12-06 20:33:06 +0300

    fix statuses

--- src/apollo/graphql/query/notificationItem.js
+++ src/apollo/graphql/query/notificationItem.js
@@ -1,9 +1,10 @@
 import faker from 'faker';
 import { userItem } from './userItem';
+import { STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL } from '../../../shared/statuses';
 
 export const notificationItem = () => ({
   id: faker.random.uuid(),
-  status: faker.random.arrayElement(['answered', 'not_answered']),
+  status: faker.random.arrayElement([STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL]),
   date: faker.date.past().toUTCString(),
   bank: userItem(),
   client: userItem(),
