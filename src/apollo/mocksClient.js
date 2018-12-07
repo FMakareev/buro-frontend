44818e46c1f56220618cf3cff708d6d281ed9ce3
NikolaZh <djozefs@gmail.com>
2018-12-07 16:17:26 +0300

    add mutation

--- src/apollo/mocksClient.js
+++ src/apollo/mocksClient.js
@@ -1,49 +1,51 @@
+import faker from 'faker';
 import setupClient from './helpers/apolloClientMock';
 import schema from './schema.graphqls';
-import {userDocumentList} from './graphql/query/userDocumentList';
-import {userList} from './graphql/query/userList';
-import {userItem} from './graphql/query/userItem';
-import {notificationList} from './graphql/query/notificationList';
-import {ROLE_BANK, ROLE_BUREAU, ROLE_CLIENT} from "../shared/roles";
-import faker from "faker";
+import { userDocumentList } from './graphql/query/userDocumentList';
+import { userList } from './graphql/query/userList';
+import { userItem } from './graphql/query/userItem';
+import { notificationList } from './graphql/query/notificationList';
+import { ROLE_BANK, ROLE_BUREAU, ROLE_CLIENT } from '../shared/roles';
 
 const defaultMocks = {
   Query: () => ({
     userList,
     userItem,
-    userEmailItem: (query, {email}) => {
+    userEmailItem: (query, { email }) => {
       console.log(query, email);
       switch (email) {
-        case('client@test.com'): {
+        case 'client@test.com': {
           return {
             ...userItem(),
             email: 'client@test.com',
             role: ROLE_CLIENT,
-          }
+          };
         }
-        case('bank@test.com'): {
+        case 'bank@test.com': {
           return {
             ...userItem(),
             email: 'bank@test.com',
             role: ROLE_BANK,
-          }
+          };
         }
-        case('bureau@test.com'): {
+        case 'bureau@test.com': {
           return {
             ...userItem(),
             email: 'bureau@test.com',
             role: ROLE_BUREAU,
-          }
+          };
         }
         default: {
-          throw Error(JSON.stringify({
-            userEmailItem: null,
-            errors: [
-              {
-                message: 'GraphQL error: user not found'
-              }
-            ]
-          }))
+          throw Error(
+            JSON.stringify({
+              userEmailItem: null,
+              errors: [
+                {
+                  message: 'GraphQL error: user not found',
+                },
+              ],
+            }),
+          );
         }
       }
     },
@@ -58,18 +60,40 @@ const defaultMocks = {
     createUser: (mutation, props) => props,
     updateUser: (mutation, props) => props,
     userResetPassword: (mutation, props) => props,
-    createNotification: (mutation, props) => {
+    createNotification: (mutation, props) =>
       // для имитации запроса к серверу с рандомной задержкой и результатом.
-      return new Promise((resolve, reject) => {
+      new Promise((resolve, reject) => {
         setTimeout(() => {
-          faker.random.number(1) ? resolve(JSON.stringify({data: {createNotification: props}})) : reject(JSON.stringify({
-            errors: [{
-              message: 'error!'
-            }]
-          }));
+          faker.random.number(1)
+            ? resolve(JSON.stringify({ data: { createNotification: props } }))
+            : reject(
+                JSON.stringify({
+                  errors: [
+                    {
+                      message: 'error!',
+                    },
+                  ],
+                }),
+              );
         }, faker.random.number(2000));
-      })
-    },
+      }),
+    updateNotification: (mutation, props) =>
+      // для имитации запроса к серверу с рандомной задержкой и результатом.
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          faker.random.number(1)
+            ? resolve(JSON.stringify({ data: { updateNotification: props } }))
+            : reject(
+                JSON.stringify({
+                  errors: [
+                    {
+                      message: 'error!',
+                    },
+                  ],
+                }),
+              );
+        }, faker.random.number(2000));
+      }),
     userPasswordRecovery: (mutation, props) =>
       // throw Error(JSON.stringify({
       //   userPasswordRecovery: null,
