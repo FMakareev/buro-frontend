046584289a45ee20b7188dffa51ffb13b1ab7d0f
NikolaZh <djozefs@gmail.com>
2018-12-07 17:41:46 +0300

    fix import

--- src/components/LayoutBase/LayoutBase.js
+++ src/components/LayoutBase/LayoutBase.js
@@ -1,21 +1,21 @@
-import React, {Fragment, Component} from 'react';
-import {connect} from 'react-redux';
-import {LAYOUT_ADMIN, LAYOUT_APP} from '../../shared/layout';
-import {Footer} from "../Footer/Footer";
+import React, { Fragment, Component } from 'react';
+import { connect } from 'react-redux';
+import { LAYOUT_ADMIN, LAYOUT_APP } from '../../shared/layout';
+import { Footer } from '../Footer/Footer';
 import styled from 'styled-components';
-import {Box} from "../Box/Box";
-import {getUserFromStore} from "../../store/reducers/user/selectors";
-import {PreloaderWrapper} from "../PreloaderWrapper/PreloaderWrapper";
-import {SpeedingWheel} from "../SmallPreloader/SmallPreloader";
-import {Text} from "../Text/Text";
-import {Header} from "../Header";
-import {HeaderNav} from "../HeaderNav/HeaderNav";
-import {HeaderNotification} from "../HeaderNotification/HeaderNotification";
+import { Box } from '../Box/Box';
+import { getUserFromStore } from '../../store/reducers/user/selectors';
+import { PreloaderWrapper } from '../PreloaderWrapper/PreloaderWrapper';
+import { SpeedingWheel } from '../SmallPreloader/SmallPreloader';
+import { Text } from '../Text/Text';
+import { Header } from '../Header/Header';
+import { HeaderNav } from '../HeaderNav/HeaderNav';
+import { HeaderNotification } from '../HeaderNotification/HeaderNotification';
 
 const MainStyled = styled(Box)`
   width: 100%;
   min-height: 100vh;
-  
+
   &:after {
     content: '';
     display: block;
@@ -23,7 +23,7 @@ const MainStyled = styled(Box)`
   }
 `;
 
-@connect((state) => ({
+@connect(state => ({
   user: getUserFromStore(state),
 }))
 export class LayoutBase extends Component {
@@ -36,7 +36,7 @@ export class LayoutBase extends Component {
 
   get initialState() {
     const {
-      route: {routes},
+      route: { routes },
       location,
     } = this.props;
     const indexRoute = this.findLayoutInPathname(location.pathname);
@@ -51,11 +51,11 @@ export class LayoutBase extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const pathname = {...this.state};
+    const pathname = { ...this.state };
 
     if (nextProps.location.pathname !== pathname) {
       const {
-        route: {routes},
+        route: { routes },
         location,
       } = nextProps;
       this.updateLayout(location, routes);
@@ -88,41 +88,35 @@ export class LayoutBase extends Component {
   };
 
   render() {
-    const {Layout, routes} = this.state;
-    const {user} = this.props;
-    return <Fragment>
-      <MainStyled>
-        <Header {...this.state} {...this.props} >
-          {
-            user && !user.initLoading && !user.error &&
-            <Box px={2}>
-              <HeaderNav user={user}/>
-            </Box>
-          }
-          {
-            user && !user.initLoading && !user.error &&
-            <Box px={2}>
-              <HeaderNotification user={user}/>
-            </Box>
-          }
-        </Header>
-        {
-          user && user.initLoading &&
-          <PreloaderWrapper>
-            <Text fontSize={12}>
-              <SpeedingWheel/>
-            </Text>
-          </PreloaderWrapper>
-        }
-        {
-          (user &&
-            !user.initLoading) &&
-          Layout &&
-          <Layout {...this.props} route={routes}/>
-        }
-      </MainStyled>
-      <Footer/>
-    </Fragment>;
+    const { Layout, routes } = this.state;
+    const { user } = this.props;
+    return (
+      <Fragment>
+        <MainStyled>
+          <Header {...this.state} {...this.props}>
+            {user && !user.initLoading && !user.error && (
+              <Box px={2}>
+                <HeaderNav user={user} />
+              </Box>
+            )}
+            {user && !user.initLoading && !user.error && (
+              <Box px={2}>
+                <HeaderNotification user={user} />
+              </Box>
+            )}
+          </Header>
+          {user && user.initLoading && (
+            <PreloaderWrapper>
+              <Text fontSize={12}>
+                <SpeedingWheel />
+              </Text>
+            </PreloaderWrapper>
+          )}
+          {user && !user.initLoading && Layout && <Layout {...this.props} route={routes} />}
+        </MainStyled>
+        <Footer />
+      </Fragment>
+    );
   }
 }
 
