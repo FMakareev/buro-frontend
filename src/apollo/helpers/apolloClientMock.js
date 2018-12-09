6508f6de2a270d2f66f6e632f2485d54e105aaaa
FMakareev <f.makareev@compaero.ru>
2018-12-09 14:44:48 +0300

    Добавил загрузку кеша при инициалиации на киленте.

--- src/apollo/helpers/apolloClientMock.js
+++ src/apollo/helpers/apolloClientMock.js
@@ -1,6 +1,6 @@
-const { ApolloClient } = require('apollo-client');
-const { InMemoryCache } = require('apollo-cache-inmemory');
-const { SchemaLink } = require('apollo-link-schema');
+const {ApolloClient} = require('apollo-client');
+const {InMemoryCache} = require('apollo-cache-inmemory');
+const {SchemaLink} = require('apollo-link-schema');
 const {
   makeExecutableSchema,
   addMockFunctionsToSchema
@@ -11,17 +11,17 @@ export function setupClient(mockResolvers, typeDefs) {
   return function createClient(overwriteMocks = {}) {
     const mergedMocks = merge({...mockResolvers}, overwriteMocks);
 
-    const schema = makeExecutableSchema({ typeDefs });
+    const schema = makeExecutableSchema({typeDefs});
     addMockFunctionsToSchema({
       schema,
       mocks: mergedMocks
     });
 
-    const apolloCache = new InMemoryCache({});
+    const apolloCache = new InMemoryCache(isBrowser ? window.APOLLO_STATE : {});
 
     return new ApolloClient({
       cache: apolloCache,
-      link: new SchemaLink({ schema })
+      link: new SchemaLink({schema})
     });
 
   }
