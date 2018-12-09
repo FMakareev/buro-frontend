7cb1a089bd20a083a3f2f62ebc44259afc53934e
FMakareev <f.makareev@compaero.ru>
2018-12-09 14:45:44 +0300

    Изменил максимальное кол-во элементов в генераторе.

--- src/apollo/graphql/query/notificationList.js
+++ src/apollo/graphql/query/notificationList.js
@@ -3,6 +3,6 @@ import { range } from '../../helpers/range';
 import { notificationItem } from './notificationItem';
 
 export const notificationList = (query, props) =>
-  range(faker.random.number(50)).map(() => notificationItem());
+  range(faker.random.number(faker.random.number(30))).map(() => notificationItem());
 
 export default notificationList;
