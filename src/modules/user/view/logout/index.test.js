93bfc1103f5c13f0c257ec5607a854ef9256e45a
NikolaZh <djozefs@gmail.com>
2018-12-12 14:16:11 +0300

    fix tests

--- src/modules/user/view/logout/index.test.js
+++ src/modules/user/view/logout/index.test.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { ApolloProvider } from 'react-apollo';
-import mocksClient from "../../../../apollo/mocksClient";
-
 import { BrowserRouter } from 'react-router-dom';
-import {LoginPage} from './index';
-import {StyledThemeProvider} from "../../../../styles/StyledThemeProvider";
-import {Provider as ProviderRedux} from 'react-redux';
+import { Provider as ProviderRedux } from 'react-redux';
+import mocksClient from '../../../../apollo/mocksClient';
+
+import { LoginPage } from './index';
+import { StyledThemeProvider } from '../../../../styles/StyledThemeProvider';
 import { Store } from '../../../../store';
 
-
 test('LoginPage: рендер без ошибок', () => {
-  const output = renderer.create(<StyledThemeProvider>
-    <ProviderRedux store={Store}>
-      <BrowserRouter>
-        <ApolloProvider client={mocksClient}>
-          <LoginPage/>
-        </ApolloProvider>
-      </BrowserRouter>
-    </ProviderRedux>
-  </StyledThemeProvider>);
+  const output = renderer.create(
+    <StyledThemeProvider>
+      <ProviderRedux store={Store}>
+        <BrowserRouter>
+          <ApolloProvider client={mocksClient}>
+            <LoginPage />
+          </ApolloProvider>
+        </BrowserRouter>
+      </ProviderRedux>
+    </StyledThemeProvider>,
+  );
   expect(output).toMatchSnapshot();
 });
-
