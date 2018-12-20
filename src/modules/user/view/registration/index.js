b2db07ee58f03a98558ba7c1bede4fcd9a724dda
FMakareev <f.makareev@compaero.ru>
2018-12-20 23:20:08 +0300

    Удолил лишние импорты

--- src/modules/user/view/registration/index.js
+++ src/modules/user/view/registration/index.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
-import { Box } from '@lib/ui/Box/Box';
-
-import { Wrapper } from '../../components/Wrapper/Wrapper';
-import { Title } from '../../components/Title/Title';
-import { HelpText } from '../../components/HelpText/HelpText';
 
 import FormUserRegistration from '../../components/FormUserRegistration/FormUserRegistration';
 
