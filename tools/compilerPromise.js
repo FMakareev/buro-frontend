0aa97fa69c4dc92b59a6c0a0ea5b088fe996efd1
FMakareev <f.makareev@compaero.ru>
2018-12-27 10:54:37 +0300

    Изменил вывод ошибки

--- tools/compilerPromise.js
+++ tools/compilerPromise.js
@@ -1,10 +1,14 @@
+import {logMessage} from "./index";
+
 export const compilerPromise = compiler =>
   new Promise(resolve => {
     compiler.plugin('done', stats => {
       if (!stats.hasErrors()) {
         return resolve();
       }
-      return Promise.reject(new Error('Compiler promise error.'));
+
+      logMessage('Compiler promise error.', 'error');
+      return Promise.reject(new Error(stats));
     });
   });
 
