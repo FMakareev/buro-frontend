bed498144dda372f7c068fa210fa4496a2f8f3f0
NikolaZh <djozefs@gmail.com>
2018-12-28 14:25:14 +0300

    update test

--- src/modules/bank/view/notifications/index.js
+++ src/modules/bank/view/notifications/index.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import dayjs from 'dayjs';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
-import {Query} from 'react-apollo';
-import {Container} from '@lib/ui/Container/Container';
-import {Text} from '@lib/ui/Text/Text';
-import {ReactTableStyled} from '@lib/ui/ReactTableStyled/ReactTableStyled';
-import {CheckAuthorization} from '@lib/ui/CheckAuthorization/CheckAuthorization';
-import {ROLE_BANK} from '@lib/shared/roles';
+import { Query } from 'react-apollo';
+import { Container } from '@lib/ui/Container/Container';
+import { Text } from '@lib/ui/Text/Text';
+import { ReactTableStyled } from '@lib/ui/ReactTableStyled/ReactTableStyled';
+import { CheckAuthorization } from '@lib/ui/CheckAuthorization/CheckAuthorization';
+import { ROLE_BANK } from '@lib/shared/roles';
 
-import {STATUS_APPROVAL, STATUS_NOT_APPROVAL, STATUS_PENDING} from '@lib/shared/statuses';
-import {Box} from '@lib/ui/Box/Box';
+import { STATUS_APPROVAL, STATUS_NOT_APPROVAL, STATUS_PENDING } from '@lib/shared/statuses';
+import { Box } from '@lib/ui/Box/Box';
 import NotificationListQuery from './NotificationListQuery.graphql';
 
-import {getUserFromStore} from '../../../../store/reducers/user/selectors';
+import { getUserFromStore } from '../../../../store/reducers/user/selectors';
 
 const has = Object.prototype.hasOwnProperty;
 
@@ -30,7 +30,8 @@ const columns = () => [
       try {
         if (has.call(props, 'client')) {
           return props.client
-            ? `${props.client.firstName || ''} ${props.client.lastName || ''} ${props.client.patronymic || ''}`
+            ? `${props.client.firstName || ''} ${props.client.lastName || ''} ${props.client
+                .patronymic || ''}`
             : null;
         }
       } catch (error) {
@@ -48,18 +49,18 @@ const columns = () => [
       </Text>
     ),
     accessor: props => {
-     try{
-       if (has.call(props, 'date')) {
-         const date = dayjs(props.date).format('DD.MM.YYYY HH:mm:ss');
-         if (date.indexOf('NaN') === -1) {
-           return date
-         }
-       }
-       return '';
-     } catch(error){
-       console.error(error);
-       return '';
-     }
+      try {
+        if (has.call(props, 'date')) {
+          const date = dayjs(props.date).format('DD.MM.YYYY HH:mm:ss');
+          if (date.indexOf('NaN') === -1) {
+            return date;
+          }
+        }
+        return '';
+      } catch (error) {
+        console.error(error);
+        return '';
+      }
     },
     filterMethod: (filter, row) =>
       row[filter.id].startsWith(filter.value) && row[filter.id].endsWith(filter.value),
@@ -67,49 +68,47 @@ const columns = () => [
   {
     id: 'RequestStatus',
     Header: 'Status',
-    Cell: props => (
-      <Text>{props.value}</Text>
-    ),
+    Cell: props => <Text>{props.value}</Text>,
     accessor: props => {
-      switch(props.status){
-        case(STATUS_NOT_APPROVAL):{
+      switch (props.status) {
+        case STATUS_NOT_APPROVAL: {
           return 'Not approval';
         }
-        case(STATUS_APPROVAL):{
+        case STATUS_APPROVAL: {
           return 'Approval';
         }
-        case(STATUS_PENDING):{
+        case STATUS_PENDING: {
           return 'Pending';
         }
       }
     },
     filterMethod: (filter, row) => {
-      switch(filter.value){
-        case(STATUS_NOT_APPROVAL):{
+      switch (filter.value) {
+        case STATUS_NOT_APPROVAL: {
           return row._original.status === STATUS_NOT_APPROVAL;
         }
-        case(STATUS_APPROVAL):{
+        case STATUS_APPROVAL: {
           return row._original.status === STATUS_APPROVAL;
         }
-        case(STATUS_PENDING):{
+        case STATUS_PENDING: {
           return row._original.status === STATUS_PENDING;
         }
-        case('all'):{
+        case 'all': {
           return true;
         }
       }
     },
-    Filter: ({ filter, onChange }) =>
+    Filter: ({ filter, onChange }) => (
       <select
         onChange={event => onChange(event.target.value)}
-        style={{ width: "100%" }}
-        value={filter ? filter.value : "all"}
-      >
+        style={{ width: '100%' }}
+        value={filter ? filter.value : 'all'}>
         <option value="all">Show All</option>
         <option value={STATUS_NOT_APPROVAL}>Not approval</option>
         <option value={STATUS_APPROVAL}>Approval</option>
         <option value={STATUS_PENDING}>Pending</option>
-      </select>,
+      </select>
+    ),
   },
 ];
 
@@ -128,7 +127,7 @@ export class NotificationsPage extends Component {
   }
 
   render() {
-    const {user} = this.props;
+    const { user } = this.props;
     return (
       <Container backgroundColor="transparent" px={6}>
         <Text fontFamily="bold" fontWeight="bold" fontSize={9} lineHeight={9} mb={7}>
@@ -139,28 +138,27 @@ export class NotificationsPage extends Component {
             query={NotificationListQuery}
             variables={{
               bankid: user.id,
-            }}>
-            {({error, data, loading}) => {
-              return (
-                <ReactTableStyled
-                  defaultFilterMethod={(filter, row) =>
-                    String(row[filter.id]).indexOf(filter.value) >= 0
-                  }
-                  data={
-                    loading
-                      ? []
-                      : data && has.call(data, 'notificationlist')
-                      ? data.notificationlist
-                      : []
-                  }
-                  loadingText={loading ? 'Loading...' : error ? 'Error...' : 'Loading...'}
-                  loading={loading}
-                  error={error}
-                  filterable
-                  columns={columns()}
-                />
-              );
             }}
+           onError={ () => {} }>
+            {({ error, data, loading }) => (
+              <ReactTableStyled
+                defaultFilterMethod={(filter, row) =>
+                  String(row[filter.id]).indexOf(filter.value) >= 0
+                }
+                data={
+                  loading
+                    ? []
+                    : data && has.call(data, 'notificationlist')
+                    ? data.notificationlist
+                    : []
+                }
+                loadingText={loading ? 'Loading...' : error ? 'Error...' : 'Loading...'}
+                loading={loading}
+                error={error}
+                filterable
+                columns={columns()}
+              />
+            )}
           </Query>
         </Box>
       </Container>
