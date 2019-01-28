d05e9fde51c35c46b1029e8c01aaf8d49ddceddf
fmakareev <fmakareev@code-artel.com>
2019-01-28 21:47:08 +0300

    sync

--- src/styles/styleProperty/OrderProperty.js
+++ src/styles/styleProperty/OrderProperty.js
@@ -1,20 +1,30 @@
-export const OrderProperty = ({ theme, order }) => {
-  if (order) {
-    if (Array.isArray(order)) {
-      let style = ``;
-      order.forEach((item, index) => {
-        if (index === 0) {
-          style += `order: ${item};`;
-        } else {
-          style += `@media(min-width: ${theme.breakpoints[index]}){
-		      order: ${item};
-		    }`;
-        }
-      });
-      return style;
-    } else if (order) {
-      return `order: ${order};`;
+export const OrderProperty = ({theme, order, ...rest}) => {
+
+  console.log('OrderProperty:', rest);
+  console.log('OrderProperty:', theme);
+  try {
+    if (order) {
+      if (Array.isArray(order)) {
+        let style = ``;
+        order.forEach((item, index) => {
+          if (index === 0) {
+            style += `order: ${item};`;
+          } else {
+            if (theme && theme.breakpoints && Array.isArray(theme.breakpoints) && index < theme.breakpoints.length) {
+              style += `@media(min-width: ${theme.breakpoints[index]}){
+		          order: ${item};
+		        }`;
+            }
+          }
+        });
+        return style;
+      } else if (order) {
+        return `order: ${order};`;
+      }
     }
+  } catch (error) {
+    console.log(error);
+    return '';
   }
 };
 
