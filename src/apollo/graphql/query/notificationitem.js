0a9de003c16cdcca147cf13931cb3953eafa5cd2
fmakareev <fmakareev@code-artel.com>
2019-01-30 16:18:55 +0300

    Change faker

--- src/apollo/graphql/query/notificationitem.js
+++ src/apollo/graphql/query/notificationitem.js
@@ -1,14 +1,27 @@
 import faker from 'faker';
-import { useritem } from './userItem';
-import { STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL } from '../../../shared/statuses';
+import {useritem} from './userItem';
+import {STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL} from '../../../shared/statuses';
+import {ROLE_BANK, ROLE_BUREAU} from "@lib/shared/roles";
 
-export const notificationitem = () => ({
-  id: faker.random.uuid(),
-  status: faker.random.arrayElement([STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL]),
-  date: faker.date.past().toUTCString(),
-  bank: useritem(),
-  message: 'Client requested a credit history.',
-  client: useritem(),
-});
+export const notificationitem = () => {
+
+  const isBureau = faker.random.boolean();
+  return ({
+    id: faker.random.uuid(),
+    status: faker.random.arrayElement([STATUS_PENDING, STATUS_APPROVAL, STATUS_NOT_APPROVAL]),
+    date: faker.date.past().toUTCString(),
+    message: 'Client requested a credit history.',
+    client: useritem(),
+    bank: isBureau ? null : {
+      ...useritem(),
+      role: ROLE_BANK,
+      bankName: ROLE_BANK,
+    },
+    bureau: isBureau ? {
+      ...useritem(),
+      role: ROLE_BUREAU
+    } : null,
+  });
+}
 
 export default notificationitem;
--- src/apollo/graphql/query/notificationlist.js
+++ src/apollo/graphql/query/notificationlist.js
@@ -3,6 +3,6 @@ import { range } from '../../helpers/range';
 import { notificationitem } from './notificationitem';
 
 export const notificationlist = (query, props) =>
-  range(faker.random.number(faker.random.number(30))).map(() => notificationitem());
+  range(100).map(() => notificationitem());
 
 export default notificationlist;
