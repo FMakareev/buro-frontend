c5be1d35793d2ff501df3d89b8c6db37986082e7
fmakareev <fmakareev@code-artel.com>
2019-02-13 15:42:39 +0300

    Add transaction list in UserDocument

--- src/apollo/graphql/query/userdocumentitem.js
+++ src/apollo/graphql/query/userdocumentitem.js
@@ -1,17 +1,19 @@
 import faker from 'faker';
-import { range } from '../../helpers/range';
-import { useritem } from './userItem';
-import { ROLE_BANK, ROLE_CLIENT } from '../../../shared/roles';
-import { STATUS_DOWNLOADED, STATUS_NEED_UPDATE } from '../../../shared/statuses';
+import {range} from '../../helpers/range';
+import {useritem} from './userItem';
+import {ROLE_BANK, ROLE_CLIENT} from '../../../shared/roles';
+import {STATUS_DOWNLOADED, STATUS_NEED_UPDATE} from '../../../shared/statuses';
+import {transactionlist} from "./transactionlist";
 
-export const userdocumentitem = (query, props) =>{
+export const userdocumentitem = (query, props) => {
   const status = faker.random.number(1) ? STATUS_DOWNLOADED : STATUS_NEED_UPDATE;
 
   return {
     id: faker.random.uuid(),
-    client: { ...useritem(), role: ROLE_CLIENT },
-    owner: { ...useritem(), role: ROLE_BANK },
+    client: {...useritem(), role: ROLE_CLIENT},
+    owner: {...useritem(), role: ROLE_BANK},
     date: faker.date.past().toISOString(),
+    transactionlist: transactionlist(),
     status,
   };
 };
